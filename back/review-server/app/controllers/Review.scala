4c3af9f0bf47ad9e27d44769274e0a6ea268b1ed
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-02-15 19:02:48 -0500

    Able to create reviews
    In the least safe way possible! Appending new review objects
    deserialized by the implicit reader to the listbuffer within the Review
    controller. But hey, this is a learning process after all.

--- back/review-server/app/controllers/Review.scala
+++ back/review-server/app/controllers/Review.scala
@@ -4,28 +4,45 @@ import play.api._
 import play.api.mvc._
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
+import scala.collection.mutable.ListBuffer
 
 
 object Review extends Controller {
 	
   case class Review(name: String, description:String)	
 
-	
-  def list = Action {
-		val reviews = List(
-			new Review("H",  "description and stuff like that")
-		)
-
-		implicit val reviewWriter = new Writes[Review] {
-			def writes(r: Review): JsValue = {
-				Json.obj(
-					"name" -> r.name,
-					"description" -> r.description
-				)
-			}
+  implicit val reviewWriter = new Writes[Review] {
+		def writes(r: Review): JsValue = {
+			Json.obj(
+				"name" -> r.name,
+				"description" -> r.description
+			)
 		}
+	}
 
-		Ok(Json.toJson(reviews))
+	implicit val reviewReader: Reads[Review] = (
+		(__ \ "name").read[String] and 
+		(__ \ "description").read[String]
+	)(Review) 
+
+	val reviews = ListBuffer[Review](
+			new Review("H",  "description and stuff like that")
+	)
+
+	def create = Action { request => 
+		request.body.asJson.map { json => 
+			json.validate[Review].map{
+				case r => reviews += r; Ok("Create")
+			}.recoverTotal {
+				e => BadRequest("Detected error:"+ JsError.toFlatJson(e))
+			}
+		}.getOrElse{
+			BadRequest("Could not parse data")
+		}
+	}
+
+  def list = Action {
+		Ok(Json.toJson(reviews.toList))
 	}
 
 }
