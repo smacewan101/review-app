15bf06623cbde6e32bf8360843df3f61ad2aeb85
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-02-15 20:09:41 -0500

    Added CORS filter
    Added Cors filter so we can submit between lan
    
    Aso changed review to title and content

--- back/review-server/app/controllers/Review.scala
+++ back/review-server/app/controllers/Review.scala
@@ -5,38 +5,39 @@ import play.api.mvc._
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
 import scala.collection.mutable.ListBuffer
+import play.api.db._
 
 
 object Review extends Controller {
 	
-  case class Review(name: String, description:String) {
+  case class Review(title: String, content:String) {
 		var id = 0 
 	}
 
   implicit val reviewWriter = new Writes[Review] {
 		def writes(r: Review): JsValue = {
 			Json.obj(
-				"name" -> r.name,
-				"description" -> r.description,
+				"title" -> r.title,
+				"content" -> r.content,
 				"id" -> r.id
 			)
 		}
 	}
 
 	implicit val reviewReader: Reads[Review] = (
-		(__ \ "name").read[String] and 
-		(__ \ "description").read[String]
+		(__ \ "title").read[String] and 
+		(__ \ "content").read[String]
 	)(Review) 
 
 	val reviews = ListBuffer[Review]()
 
 	var idx = 2
 
-	def show(id: Int) = Action { 
+	def show(id: Int) =  Action { 
 		Ok(Json.toJson(reviews.find(review => review.id == id)))
 	}
 
-	def create = Action { request => 
+	def create =  Action { request => 
 		request.body.asJson.map { json => 
 			json.validate[Review].map{
 				case r => { 
@@ -53,9 +54,14 @@ object Review extends Controller {
 		}
 	}
 
-  def list = Action {
+  def list =  Action {
 		Ok(Json.toJson(reviews.toList))
 	}
 
-
+	def preflight(all: String) = Action {
+	  Ok("").withHeaders("Access-Control-Allow-Origin" -> "*",
+	    "Allow" -> "*",
+	    "Access-Control-Allow-Methods" -> "POST, GET, PUT, DELETE, OPTIONS",
+	    "Access-Control-Allow-Headers" -> "Origin, X-Requested-With, Content-Type, Accept, Referrer, User-Agent");
+	 }
 }
